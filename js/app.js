631d3138f6eea731c0be32178e0691931eb80730
Jure Cuhalev <jure@cuhalev.com>
2017-01-03 14:21:15 +0100

    Fix perks

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
+/*globals $:false, ScrollMagic:false, Linear:false */
+
 $(document).foundation();
+// var controller = new ScrollMagic.Controller();
+
+// var scene = new ScrollMagic.Scene({
+// 					triggerElement: ".animation-mug", duration: $(window).height()
+// 				})
+// 				.setTween('.animation-monitors', {left: '-=100vh', ease: Linear.easeNone})
+// 				.addIndicators()
+// 				.addTo(controller);
+
+// console.log(scene);

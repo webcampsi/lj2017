7f30bc5c412863fed4db8f72097a07abae344e3c
Jure Cuhalev <jure@cuhalev.com>
2017-01-10 19:26:16 +0100

    Correctly build CSS

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ var minify = require('gulp-minify');
 var concat = require('gulp-concat');
 
 var sassPaths = [
-  'assets/bower_components/normalize.scss/sass',
-  'assets/bower_components/foundation-sites/scss',
-  'assets/bower_components/motion-ui/src'
+  'bower_components/normalize.scss/sass',
+  'bower_components/foundation-sites/scss',
+  'bower_components/motion-ui/src'
 ];
 
 gulp.task('sass', function() {
-  return gulp.src('assets/scss/app.scss')
+  return gulp.src('scss/app.scss')
     .pipe($.sass({
       includePaths: sassPaths,
       outputStyle: 'compressed' // if css compressed **file size**
@@ -45,3 +45,5 @@ gulp.task('compress', function() {
 gulp.task('default', ['sass'], function() {
   gulp.watch(['scss/**/*.scss'], ['sass']);
 });
+
+gulp.task('build', ['sass', 'compress']);

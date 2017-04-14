c9e062632555f00598298737136afe14b5ee28a1
Mateja Verlic <mateja.verlic@gmail.com>
2017-04-14 12:10:11 +0200

    Added initialization for styles on the day of event

--- js/schedule.js
+++ js/schedule.js
@@ -4,34 +4,45 @@ var WebCamp = window.WebCamp || {};
 
     'use strict';
 
+    var mainEventDate = null;
+    var currentDate = null;
+    var setStylesEnabled = false;
+
     function getClassForSectionInProgress(slotStartTime, slotEndTime) {
 
+        const SECONDS_IN_HOUR = 3600;
+        const SECONDS_IN_MINUTE = 60;
+        const PAST_SLOT_CLASS = 'past';
+        const ACTIVE_SLOT_CLASS = 'active-now';
+        const FUTURE_SLOT_CLASS = 'future';
+
+        var slotStart, slotEnd;
+        var slotClass = FUTURE_SLOT_CLASS;
         var slotStartTimeParts = slotStartTime.split(':');
         var slotEndTimeParts = slotEndTime.split(':');
 
-        var secsInHour = 3600;
-        var secsInMinute = 60;
+        var hours = currentDate.getHours();
+        var minutes = currentDate.getMinutes();
+        var currentTime = hours * SECONDS_IN_HOUR + minutes * SECONDS_IN_MINUTE;
 
-        var slotStart = slotStartTimeParts[0]*secsInHour + slotStartTimeParts[1]*secsInMinute;
-        var slotEnd = slotEndTimeParts[0]*secsInHour + slotEndTimeParts[1]*secsInMinute;
+        if (slotStartTimeParts.lenght < 2 || slotEndTimeParts.lenght < 2) {
+            return;
+        }
 
-        var hours = 11;//new Date().getHours();
-        var minutes = 35; //newDate.getMinutes();
-        var currentTime = hours*secsInHour + minutes*secsInMinute;
-
-        var slotClass = 'future';
+        slotStart = slotStartTimeParts[0] * SECONDS_IN_HOUR + slotStartTimeParts[1] * SECONDS_IN_MINUTE;
+        slotEnd = slotEndTimeParts[0] * SECONDS_IN_HOUR + slotEndTimeParts[1] * SECONDS_IN_MINUTE;
 
         if (currentTime >= slotEnd) {
-            slotClass = 'past';
+            slotClass = PAST_SLOT_CLASS;
 
         } else if (currentTime >= slotStart) {
-            slotClass = 'active-now'
+            slotClass = ACTIVE_SLOT_CLASS;
         }
 
         return slotClass;
     }
 
-    var setSlotStyles = function setSlotStyles() {
+    var applySlotStyles = function applySlotStyles() {
 
         $("tr.slot").each(function(){
             var slotStarts = $(this).find(".slot-time").data("start-time");
@@ -42,7 +53,27 @@ var WebCamp = window.WebCamp || {};
           });
     }
 
+    var init = function init(eventDate) {
+        mainEventDate = new Date(eventDate);
+        currentDate = new Date();
+
+        var tmpEventDay = new Date(mainEventDate);
+        var tmpCurrentDay = new Date(currentDate);
+
+        tmpEventDay.setHours(0, 0, 0, 0);
+        tmpCurrentDay.setHours(0, 0, 0, 0);
+
+        if(tmpCurrentDay.getTime() == tmpEventDay.getTime()) {
+            setStylesEnabled = true;
+        }
+
+        if (setStylesEnabled) {
+            applySlotStyles();
+        }
+    }
+
     WebCamp.Schedule = {};
-    WebCamp.Schedule.setStyles = setSlotStyles;
+    WebCamp.Schedule.init = init;
+    WebCamp.Schedule.setStyles = applySlotStyles;
 
 }(jQuery, WebCamp));
